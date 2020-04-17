3c73869601e659d08a400551baea5ed685435844
Cyril Lugan <cyril@lugan.fr>
2020-04-17 19:08:15 +0200

    Guessing the current values order

--- data_processing.py
+++ data_processing.py
@@ -2,6 +2,12 @@
 
 import threading
 import datetime as dt
+import os
+import time
+import json
+
+CACHE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "cache.json")
+CACHE_WRITE_PERIOD_MIN = 10
 
 class DataProcessing():
 
@@ -10,12 +16,43 @@ class DataProcessing():
         self.lock = threading.Lock()
 
     def on_new_data(self, data):
+        data = self.process(data)
         with self.lock:
             self.lastest_data = data
-            self.lastest_data.insert(0, dt.datetime.utcnow())
 
     def pop_latest_data(self):
         with self.lock:
             data = self.lastest_data
             self.lastest_data = None
             return data
+
+    # Données utiles, préconisations:
+    #
+    # Vecs: volume d’eau froide à l’entrée du ballon solaire
+    # Tef: température de l’eau froide
+    # Tec: température de l’eau chaude directement à la sortie du ballon
+    # TODO Consommation électrique
+
+    def process(self, data):
+
+        values = data[2:] # discard the first values (ticks converted to liters)
+        labels = [
+            'Vecs_ticks',  # TODO lequel est Vecs?
+            'Vep_ticks',
+            'Teh', # Haut du ballon
+            'Teb', # Bas du ballon
+            'Tec', # Eau chaude
+            'Tef', # Eau froide
+            'T5',  # TODO Eau sortant du circuit des panneaux (code arduino) vs Température des panneaux (rapport)
+            'T6',  # TODO Entrée échangeur?
+            'T7',  # TODO Sortie échangeur?
+        ]
+        data = dict(zip(labels, values))
+
+        # TODO rapport de stage 1 tick = 0.25 litre, pas cohérent avec les valeurs dans le code
+        # 1 tick = 0.25 liter
+        data['Vecs'] = data['Vecs_ticks'] / 4
+        data['Vep']  = data['Vep_ticks']  / 4
+
+        data['Stamp_utc'] = dt.datetime.utcnow()
+        return data
